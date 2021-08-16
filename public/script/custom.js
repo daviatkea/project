ea2a3aa4db611b23efcc8749858817c39303be36
Dannie Vinther <davi@kea.dk>
2021-08-16 16:04:52 +0200

    update regex again

--- public/script/custom.js
+++ public/script/custom.js
@@ -25,7 +25,8 @@
   // const classReg = /(?=.)[^\}]+(?=\{)/g;
   // const classReg = /(?! )(?=.)[^\}\,]+(?=.\{|\,)/g;
   // const classReg = /(?!\s)\w?[^{}]+(?=\s)(?=(((?!\}).)*\{)|[^\{\}]+$)/g;
-  const classReg = /(?! )(?=.)[^},\(\/*]+[^\{\}](?={|,\n|,.+,)/g;
+  // const classReg = /(?! )(?=.)[^},\(\/*]+[^\{\}](?={|,\n|,.+,)/g;
+  const classReg = /(?! )(?=.)[^},\/*]+[^\{\}](?=.{|,\n|,.+,)/g;
 
   outputs.forEach((output) => {
     output.addEventListener("click", ({ target }) => {
